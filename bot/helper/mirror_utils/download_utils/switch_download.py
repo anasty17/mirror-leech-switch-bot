565cfa765163c5526c2720e5a4e031f1682c116b
Dawn India <z-mirror@dawn-in.eu.org>
2023-11-23 21:30:24 +0530

    Update switch_download.py

--- bot/helper/mirror_utils/download_utils/switch_download.py
+++ bot/helper/mirror_utils/download_utils/switch_download.py
@@ -88,7 +88,10 @@ class SwitchDownloadHelper:
                 download = media.source_id not in GLOBAL_GID
 
             if download:
-                name = media.description
+                if not filename:
+                    name = media.description
+                else:
+                    name = filename
                 path = path + name
                 size = media.file_size
                 gid = media.source_id
