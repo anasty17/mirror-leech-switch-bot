c427abe7775a36dbcb4b1753f17d884eb22861e8
Dawn India <dawn-in@z-mirror.eu.org>
2024-08-18 16:09:40 +0530

    Terabox fix
    Support for only videos(file/folders)
    
    Signed-off-by: Dawn India <dawn-in@z-mirror.eu.org>

--- bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
@@ -5,7 +5,7 @@ from json import loads
 from lxml.etree import HTML
 from os import path as ospath
 from re import findall, match, search
-from requests import Session, post, get
+from requests import Session, post, get, RequestException
 from requests.adapters import HTTPAdapter
 from time import sleep
 from urllib.parse import parse_qs, urlparse
@@ -135,6 +135,7 @@ def direct_link_generator(link):
             "terabox.app",
             "gibibox.com",
             "goaibox.com",
+            "terasharelink.com",
         ]
     ):
         return terabox(link)
@@ -531,91 +532,87 @@ def uploadee(url):
         raise DirectDownloadLinkException("ERROR: Direct Link not found")
 
 
-def terabox(url):
-    if not ospath.isfile("terabox.txt"):
-        raise DirectDownloadLinkException("ERROR: terabox.txt not found")
-    try:
-        jar = MozillaCookieJar("terabox.txt")
-        jar.load()
-    except Exception as e:
-        raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
-    cookies = {}
-    for cookie in jar:
-        cookies[cookie.name] = cookie.value
-    details = {"contents": [], "title": "", "total_size": 0}
-    details["header"] = " ".join(f"{key}: {value}" for key, value in cookies.items())
+def terabox(url, video_quality="HD Video", save_dir="HD_Video"):
+    """Terabox direct link generator
+    https://github.com/Dawn-India/Z-Mirror"""
 
-    def __fetch_links(session, dir_="", folderPath=""):
-        params = {"app_id": "250528", "jsToken": jsToken, "shorturl": shortUrl}
-        if dir_:
-            params["dir"] = dir_
-        else:
-            params["root"] = "1"
+    pattern = r"/s/(\w+)|surl=(\w+)"
+    if not search(pattern, url):
+        raise DirectDownloadLinkException("ERROR: Invalid terabox URL")
+
+    netloc = urlparse(url).netloc
+    terabox_url = url.replace(
+        netloc,
+        "1024tera.com"
+    )
+
+    urls = [
+        "https://ytshorts.savetube.me/api/v1/terabox-downloader",
+        f"https://teraboxvideodownloader.nepcoderdevs.workers.dev/?url={terabox_url}",
+        f"https://terabox.udayscriptsx.workers.dev/?url={terabox_url}"
+    ]
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/json",
+        "Origin": "https://ytshorts.savetube.me",
+        "Alt-Used": "ytshorts.savetube.me",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin"
+    }
+
+    for base_url in urls:
         try:
-            _json = session.get(
-                "https://www.1024tera.com/share/list",
-                params=params,
-                cookies=cookies,
-            ).json()
-        except Exception as e:
-            raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
-        if _json["errno"] not in [0, "0"]:
-            if "errmsg" in _json:
-                raise DirectDownloadLinkException(f"ERROR: {_json['errmsg']}")
+            if "api/v1" in base_url:
+                response = post(
+                    base_url,
+                    headers=headers,
+                    json={"url": terabox_url}
+                )
             else:
-                raise DirectDownloadLinkException("ERROR: Something went wrong!")
+                response = get(base_url)
 
-        if "list" not in _json:
-            return
-        contents = _json["list"]
-        for content in contents:
-            if content["isdir"] in ["1", 1]:
-                if not folderPath:
-                    if not details["title"]:
-                        details["title"] = content["server_filename"]
-                        newFolderPath = ospath.join(details["title"])
-                    else:
-                        newFolderPath = ospath.join(
-                            details["title"], content["server_filename"]
-                        )
-                else:
-                    newFolderPath = ospath.join(folderPath, content["server_filename"])
-                __fetch_links(session, content["path"], newFolderPath)
-            else:
-                if not folderPath:
-                    if not details["title"]:
-                        details["title"] = content["server_filename"]
-                    folderPath = details["title"]
-                item = {
-                    "url": content["dlink"],
-                    "filename": content["server_filename"],
-                    "path": ospath.join(folderPath),
-                }
-                if "size" in content:
-                    size = content["size"]
-                    if isinstance(size, str) and size.isdigit():
-                        size = float(size)
-                    details["total_size"] += size
-                details["contents"].append(item)
+            if response.status_code == 200:
+                break
+        except RequestException as e:
+            raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}") from e
+    else:
+        raise DirectDownloadLinkException("ERROR: Unable to fetch the JSON data")
 
-    with Session() as session:
-        try:
-            _res = session.get(url, cookies=cookies)
-        except Exception as e:
-            raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}")
-        if jsToken := findall(r"window\.jsToken.*%22(.*)%22", _res.text):
-            jsToken = jsToken[0]
-        else:
-            raise DirectDownloadLinkException("ERROR: jsToken not found!.")
-        shortUrl = parse_qs(urlparse(_res.url).query).get("surl")
-        if not shortUrl:
-            raise DirectDownloadLinkException("ERROR: Could not find surl")
-        try:
-            __fetch_links(session)
-        except Exception as e:
-            raise DirectDownloadLinkException(e)
+    data = response.json()
+    details = {
+        "contents": [],
+        "title": "",
+        "total_size": 0
+    }
+
+    for item in data["response"]:
+        title = item["title"]
+        resolutions = item.get(
+            "resolutions",
+            {}
+        )
+        zlink = resolutions.get(video_quality)
+        if zlink:
+            details["contents"].append({
+                "url": zlink,
+                "filename": title,
+                "path": ospath.join(
+                    title,
+                    save_dir
+                )
+            })
+        details["title"] = title
+
+    if not details["contents"]:
+        raise DirectDownloadLinkException("ERROR: No valid download links found")
+
     if len(details["contents"]) == 1:
         return details["contents"][0]["url"]
+
     return details
 
 
